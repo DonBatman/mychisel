fdb12e375a6e79342692d09a74b17eb308b795bd
DonBatman <serfdon@gmail.com>
2026-01-11 20:00:47 -0800

    Added lucky block support

--- init.lua
+++ init.lua
@@ -5,10 +5,11 @@ dofile(minetest.get_modpath("mychisel").."/chisel.lua")
 dofile(minetest.get_modpath("mychisel").."/nodes.lua")
 
 
-lucky_block:add_blocks({
-	{"dro", {"mychisel:chisel"}, 1},
-})
-
+if core.get_modpath("lucky_block") then
+	lucky_block:add_blocks({
+		{"dro", {"mychisel:chisel"}, 1},
+	})
+end
 
 
 
